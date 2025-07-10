f14fb81ce26b8b30f305acda90a4231da20546c2
Ankit-Tripathi-07 <ankit9696tripathi@gmail.com>
2025-07-10 12:51:54 +0000

    data type conversion

--- 01_basics/03_dataConversion.js
+++ 01_basics/03_dataConversion.js
@@ -11,16 +11,46 @@
 //  "33" => 33
 // "33abc" => NaN
 // true => 1;false=>0
- let isLoggedIn = 1
- let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(typeof booleanIsLoggedIn);
+//  let isLoggedIn = 1
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(typeof booleanIsLoggedIn);
 
 //  "" => false 
 // "abs" => true
 
-let someNumber = 89
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber = 89
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+// **********************************Operations************************************************************//
+// let value = 3 
+// let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " ankit"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+ 2+ "2");
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
+
+
 
  
 
